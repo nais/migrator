2deb81c6285af24a0f613f858461f978edd03a9b
Kim Tore Jensen <kimtjen@gmail.com>
2019-10-18 10:28:49 +0200

    set up pflag and arguments

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import flag "github.com/spf13/pflag"
 
+type Config struct {
+	Fasit         bool
+	FasitUser     string
+	FasitPassword string
+	FasitUrl      string
+}
+
+var (
+	cfg = Config{
+		FasitUrl: "http://localhost:8080",
+	}
+)
+
+func init() {
+	flag.BoolVar(&cfg.Fasit, "fasit", cfg.Fasit, "use fasit integration")
+	flag.StringVar(&cfg.FasitUser, "fasit-user", cfg.FasitUser, "fasit username")
+	flag.StringVar(&cfg.FasitPassword, "fasit-password", cfg.FasitPassword, "fasit password")
+}
+
+func main() {
+	flag.Parse()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nais/migrator
 
+require github.com/spf13/pflag v1.0.5
+
 go 1.13
