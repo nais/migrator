9988a86f4e6151bff9fd461f4c18d86c84ab42df
Kim Tore Jensen <kimtjen@gmail.com>
2019-10-18 11:59:18 +0200

    provide basic conversion without fasit integration

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -1,8 +1,18 @@
 package main
 
-import flag "github.com/spf13/pflag"
+import (
+	"fmt"
+	"github.com/nais/migrator/mapper"
+	"github.com/nais/migrator/models/naisd"
+	"github.com/nais/migrator/models/naiserator"
+	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
+	"os"
+)
 
 type Config struct {
+	Application   string
 	Fasit         bool
 	FasitUser     string
 	FasitPassword string
@@ -16,11 +26,48 @@ var (
 )
 
 func init() {
+	flag.StringVar(&cfg.Application, "application", cfg.Application, "application name")
 	flag.BoolVar(&cfg.Fasit, "fasit", cfg.Fasit, "use fasit integration")
 	flag.StringVar(&cfg.FasitUser, "fasit-user", cfg.FasitUser, "fasit username")
 	flag.StringVar(&cfg.FasitPassword, "fasit-password", cfg.FasitPassword, "fasit password")
 }
 
 func main() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors:    false,
+		DisableTimestamp: false,
+	})
+	log.SetOutput(os.Stderr)
 	flag.Parse()
+
+	err := run()
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var err error
+	var deploy naisd.Deploy
+	var manifest naisd.NaisManifest
+	var application naiserator.Application
+
+	decoder := yaml.NewDecoder(os.Stdin)
+	err = decoder.Decode(&manifest)
+	if err != nil {
+		return fmt.Errorf("decode input: %s", err)
+	}
+
+	deploy.Application = cfg.Application
+
+	application = mapper.Convert(manifest, deploy)
+
+	encoder := yaml.NewEncoder(os.Stdout)
+	err = encoder.Encode(application)
+	if err != nil {
+		return fmt.Errorf("encode output: %s", err)
+	}
+
+	return nil
 }
